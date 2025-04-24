c53013594ccde2a330a8db8cdef051f7abfc64e9
Vexer89 <krzysztof.ferda@gmail.com>
2025-04-24 20:47:06 +0200

    current User section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import LeftBar from "./components/leftBar";
 import "./index.css";
+import CurrentUser from "./components/currentUser";
 
 function App() {
-  return <LeftBar />;
+  return (
+    <>
+      <LeftBar />
+      <CurrentUser />
+    </>
+  );
 }
 
 export default App;
