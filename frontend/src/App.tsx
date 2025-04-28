70e320175af7eb68a14dfceef020d151b42f414e
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-04-28 17:25:59 +0200

    First try to integrate it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate} from 'react-router-dom';
 import { useState } from "react";
 import LeftBar from "./components/leftBar/leftBar";
 import CurrentUser from "./components/currentUser/currentUser";
@@ -7,25 +7,46 @@ import "./index.css";
 import "./App.css";
 import MainContent from "./segments/MainContent";
 import Notifications from './segments/Notifications';
+import Profile from './components/ui/Profile/Profile';
+import Login from './components/ui/pages/Login';
+import Register from './components/ui/pages/Register';
+
 
 function App() {
+  // const [isAuthenticated, setIsAuthenticated] = useState(true);  // Stan logowania
+
+  // // Funkcja do obsługi logowania / wylogowywania
+  // const handleLogin = () => setIsAuthenticated(true);
+  // const handleLogout = () => setIsAuthenticated(false);
+
   return (
     <div className="appContainer">
-      <div className="leftSection">
-        <LeftBar />
-        <CurrentUser />
-      </div>
-      <div className="mainContent">
+        <div className="mainLayout">
+          <div className="leftSection">
+            <LeftBar />
+            <CurrentUser />
+          </div>
+          <div className="mainContent">
+            <Routes>
+              <Route path="/" element={<MainContent />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/profile" element={<Profile />} />
+              {/* Możliwość wylogowania
+              <Route path="/logout" element={<button onClick={handleLogout}>Logout</button>} /> */}
+            </Routes>
+          </div>
+          <div className="rightMenu">
+            <RightMenu />
+          </div>
+        </div>
         <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="*" element={<Navigate to="/login" />} /> */}
         </Routes>
-      </div>
-      <div className="rightMenu">
-        <RightMenu />
-      </div>
     </div>
   );
 }
+
 
 export default App;
