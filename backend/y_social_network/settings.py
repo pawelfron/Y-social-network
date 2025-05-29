775d0edbde86e4900aaab81045cf2911360cb718
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-05-29 14:50:44 +0200

    Dockerized backend

--- backend/y_social_network/settings.py
+++ backend/y_social_network/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
