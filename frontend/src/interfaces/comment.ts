8cd9c889f6ec453cf1255c71bd38a11111229f65
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-05-29 19:36:04 +0200

    All services completed

--- frontend/src/interfaces/comment.ts
+++ frontend/src/interfaces/comment.ts
@@ -4,12 +4,20 @@ export interface Comment {
   id: number;
   post: number;
   content: string;
-  created_at: string;
-  updated_at: string;
+  created_at: Date;
+  updated_at: Date;
   author: UserSummary;
 }
 
 export interface CommentCreateData {
   post: number;
   content: string;
+}
+
+export interface CommentWithID extends CommentCreateData {
+    id: number;
+}
+
+export interface CommentContent {
+    content: string;
 }
---
+++ frontend/src/services/commentsService.ts
@@ -0,0 +1,29 @@
+import { CommentContent, CommentCreateData, CommentWithID } from "../interfaces/comment";
+import { axiosInstance } from "./apiClient"
+
+export const CommentsService = {
+
+    async getPostComments(postId: number) : Promise<Comment[]>{
+        const res = await axiosInstance.get(`/posts/${postId}/comments`);
+        return res.data;
+    },
+
+    async addComment(commentData: CommentCreateData): Promise<CommentWithID>{
+        const res = await axiosInstance.post(`/comments`, commentData);
+        return res.data;
+    },
+
+    async getComment(commentId: number): Promise<Comment> {
+        const res = await axiosInstance.get(`/commnents/${commentId}`);
+        return res.data;
+    },
+
+    async editComment(commentId: number, commentContent: CommentContent): Promise<CommentContent> {
+        const res = await axiosInstance.put(`/comments/${commentId}/edit`, commentContent);
+        return res.data
+    },
+
+    async deleteComment(commentId: number){
+        await axiosInstance.delete(`/comments/${commentId}/delete`);
+    }
+}
