c1ef17e7cbdaaf5cfb55cfcc54aa34d338912e36
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-06-02 16:53:06 +0200

    Added mobile view

--- frontend/src/segments/app/LeftBar.tsx
+++ frontend/src/segments/app/LeftBar.tsx
@@ -4,6 +4,9 @@ import "./LeftBar.css";
 import logo from "../../assets/Ylogo.jpg";
 import { AuthService } from "../../services/authService";
 import CreatePost from "../../components/Post/CreatePost";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+
 
 interface ProfileProps {
   onOpenModal: (content: React.ReactNode) => void;
@@ -18,38 +21,45 @@ const LeftBar : React.FC<ProfileProps> = ({ onOpenModal })=> {
   const authService = AuthService.get_instance();
   const currentUserId = authService.getUserId();
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div className="leftbar">
       <img src={logo} alt="Logo" className="logo" />
-      <div className="leftbar-links">
-        <Link to="/" className="leftbar-link">
+
+     
+      <button className="hamburger" onClick={toggleMenu} aria-label="Menu">
+          <Menu size={40} />
+      </button>
+
+      <div className={`leftbar-links ${isMobileMenuOpen ? "open" : ""}`}>
+        <Link to="/" className="leftbar-link" onClick={() => setIsMobileMenuOpen(false)}>
           <Home size={20} />
           <span>Home</span>
         </Link>
-        <Link to="/explore" className="leftbar-link">
+        <Link to="/explore" className="leftbar-link" onClick={() => setIsMobileMenuOpen(false)}>
           <Search size={20} />
           <span>Explore</span>
         </Link>
-        {/* <Link to="/notifications" className="leftbar-link">
-          <Bell size={20} />
-          <span>Notifications</span>
-        </Link> */}
-        {/* <Link to="/bookmarks" className="leftbar-link">
-          <Bookmark size={20} />
-          <span>Bookmarks</span>
-        </Link> */}
-        <Link to={`/profile/${currentUserId}`} className="leftbar-link">
+        <Link to={`/profile/${currentUserId}`} className="leftbar-link" onClick={() => setIsMobileMenuOpen(false)}>
           <User size={20} />
           <span>Profile</span>
         </Link>
-        <Link to="/settings" className="leftbar-link">
+        <Link to="/settings" className="leftbar-link" onClick={() => setIsMobileMenuOpen(false)}>
           <Settings size={20} />
           <span>Settings</span>
         </Link>
       </div>
+
+      
       <button className="post-button" onClick={addPost}>
         Post
       </button>
+
     </div>
   );
 }
