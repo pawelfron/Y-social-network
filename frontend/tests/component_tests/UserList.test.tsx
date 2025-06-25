916e841d705ccb49b7bd824cf1eca38d70c0818e
Ayasmaa <115502472+Ayasmaa@users.noreply.github.com>
2025-06-25 22:15:26 +0200

    Create UserList.test.tsx

---
+++ frontend/tests/component_tests/UserList.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import UserList from '../../../src/components/rightSection/UserList';
+import { BrowserRouter } from 'react-router-dom';
+
+jest.mock('../../../src/components/rightSection/UserCard', () => (props: any) => {
+  return <div data-testid="user-card">{props.username}</div>;
+});
+
+const mockUsers = [
+  { id: 1, username: 'user1', profile_photo: null },
+  { id: 2, username: 'user2', profile_photo: null },
+];
+
+describe('<UserList />', () => {
+  test('renders correct number of UserCards', () => {
+    render(
+      <BrowserRouter>
+        <UserList users={mockUsers} />
+      </BrowserRouter>
+    );
+
+    const cards = screen.getAllByTestId('user-card');
+    expect(cards).toHaveLength(mockUsers.length);
+    expect(screen.getByText('user1')).toBeInTheDocument();
+    expect(screen.getByText('user2')).toBeInTheDocument();
+  });
+});
